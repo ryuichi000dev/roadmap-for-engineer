a3bae84b964a5617ed9ffbe0f3a73efd1778a930
Saitoh-T <i.am.makkoi0514@gmail.com>
2023-12-01 17:23:44 +0900

    add func to change texture -To be exact, this func change the model

--- src/components/ModelViewer/ModelViewer.tsx
+++ src/components/ModelViewer/ModelViewer.tsx
@@ -1,24 +1,64 @@
-import React from 'react';
-import { Canvas } from '@react-three/fiber';
+import * as THREE from 'three';
+import React, { useRef, useEffect, useState } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { GLTFLoader, GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 import { useLoader } from '@react-three/fiber';
+import changeTexture from './ModelChanger'
+import { TextureLoader } from 'three';
 
-const Model = () => {
-    const glb = useLoader(GLTFLoader, '/models/supporters.glb'); // モデルへのパスを指定
+const ModelLoader = ({Models}:{Models:{Human: string; Desk: string; PC: string; Floor: string;}}) => {
+    const [Human,Desk,PC,Floor] = useLoader(GLTFLoader, [Models.Human,Models.Desk,Models.PC,Models.Floor]); // モデルへのパスを指定
+    return(
+    <>
+        <primitive object={Human.scene} />
+        <primitive object={Desk.scene} />
+        <primitive object={PC.scene} />
+        <primitive object={Floor.scene} />
+        <AnimeModel glb={Human} />
+    </>
+    );
+};
 
-    return <primitive object={glb.scene} />;
+const AnimeModel: React.FC<{glb:GLTF}> = ({ glb }) => {
+    const modelRef = useRef<THREE.Object3D>();
+    const mixer = new THREE.AnimationMixer(glb.scene);
+    const action = mixer.clipAction(glb.animations[0]);
+
+    useEffect(() => {
+        action.play();
+    }, [action]);
+
+    useFrame((state, delta) => {
+        mixer.update(delta);
+    });
+
+    return <primitive object={glb.scene} ref={modelRef} />;
 };
 
 const ModelViewer = () => {
+    const scene=new THREE.Scene();
+    const bgtexture = useLoader(TextureLoader,'/models/SteveLv1.png');
+    scene.background=bgtexture;
+    const [Models,setModel]=useState({
+        Human:'/models/Human.glb',
+        Desk:'/models/DeskLv1.glb',
+        PC:'/models/PC.glb',
+        Floor:'/models/Floor.glb'})
+        
     return (
         <div style={{ height: '400px', width: '100%' }}>
-            <Canvas>
-                <ambientLight intensity={0.5} />
-                <spotLight position={[10, 15, 10]} angle={0.3} />
-                <OrbitControls />
-                <Model />
+            <Canvas 
+            shadows={'basic'}
+            camera={{fov: 45, near: 0.1, far: 1000, position: [0, 3, 10]}} 
+            style={{ backgroundImage:`url(/models/backgroundImage.png)` }}>
+                <ambientLight intensity={2} />
+                <spotLight position={[0, 0, 10]}/>
+                <OrbitControls autoRotate autoRotateSpeed={-2} />
+                <ModelLoader Models={Models}/>
             </Canvas>
+            <button onClick={()=>changeTexture({Models},setModel,{Human:'/models/HumanLv2.glb'})}>Mock:Human</button>
+            <button onClick={()=>changeTexture({Models},setModel,{Desk:'/models/DeskLv2.glb'})}>Mock:Desk</button>
         </div>
     );
 };
